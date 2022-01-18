f093f75947eef0a72f9e763f338fd6b13cb3d7f9
master <devgeorg222@gmail.com>
2022-01-18 21:56:03 +0200

    Updates

---
+++ precache-manifest.159f8d7a3a9e11001bf1a420e0088074.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76ce9f2d365e1139fa55bcca46479a5d",
+    "url": "/Speech/index.html"
+  },
+  {
+    "revision": "d5028755ce9eaedef001",
+    "url": "/Speech/static/css/main.2a3d0a26.chunk.css"
+  },
+  {
+    "revision": "ac557f0ed11fe5c1dc6c",
+    "url": "/Speech/static/js/2.9d400f63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Speech/static/js/2.9d400f63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5028755ce9eaedef001",
+    "url": "/Speech/static/js/main.eb0390e5.chunk.js"
+  },
+  {
+    "revision": "1f46bcf9239e5733b0b2",
+    "url": "/Speech/static/js/runtime-main.01186912.js"
+  }
+]);
